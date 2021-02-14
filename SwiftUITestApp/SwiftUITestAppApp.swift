18627dc2860534870e794771b55a5ab08e84d23d
lee <lyhdra99@svcorps.dev>
2021-02-14 20:40:37 +0900

    init

---
+++ SwiftUITestApp/SwiftUITestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestAppApp.swift
+//  SwiftUITestApp
+//
+//  Created by leedev on 2021/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
